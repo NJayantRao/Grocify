003d26d859b0546ef4de338e5511c0ad3c17a517
N Jayant Rao <njayantrao@gmail.com>
2026-06-03 22:17:40 +0530

    Initial commit
    Generated by create-expo-app 4.0.0.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,15 @@
-import { ClerkProvider } from '@clerk/expo'
-import { tokenCache } from '@clerk/expo/token-cache'
-import { Slot } from 'expo-router'
-import "../../global.css"
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
 
-if (!publishableKey) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
-
-export default function RootLayout() {
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Slot />
-    </ClerkProvider>
-  )
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </ThemeProvider>
+  );
 }
