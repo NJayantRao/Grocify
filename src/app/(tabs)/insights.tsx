8a31d81a2d970ffc9ba38672d98e5c45389dd949
N Jayant Rao <njayantrao@gmail.com>
2026-06-07 08:44:55 +0530

    feature: Implement insights screen with category, stats, and priority sections

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import InsightsCategorySection from "@/components/InsightsCategory";
+import InsightsCompletedSection from "@/components/InsightsCompleted";
+import InsightsPrioritySection from "@/components/InsightsPriorities";
+import InsightsStatsSection from "@/components/InsightsStats";
+import TabScreenBackground from "@/components/TabScreenBg";
+import UserProfileCard from "@/components/UserProfileCard";
+import { ScrollView } from "react-native";
 
-const insights = () => {
+const InsightsScreen = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
+        <UserProfileCard />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <InsightsCompletedSection />
+      </ScrollView>
+    </>
   );
 };
 
-export default insights;
+export default InsightsScreen;
